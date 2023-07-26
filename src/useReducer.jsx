61b191ad63d88ca85e584c090f074fdd64c6beb1
veekay17 <vijay.vuex@gmail.com>
2023-07-26 15:19:35 +0530

    useReducer example

--- src/useReducer.jsx
+++ src/useReducer.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import { useReducer } from "react";
 import { data } from "./data";
+
+//its conventional to store actions in UpperCase variables
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
+  }
+  if (action.type === REMOVE_ITEM) {
+    // console.log(action);
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { ...state, people: newPeople };
+  }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+  throw new Error(`No Matching ${action.type} action type`);
+};
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // we need id to remove the item so we will pass id in payload
+    //we can direct pass id without payload but its not conventional
+    dispatch({ type: REMOVE_ITEM, payload: { id } }); //we can pass direct id to payload but its a convention to store payload's value in object
   };
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+  };
+  const resetList = () => {
+    dispatch({ type: RESET_LIST });
+  };
+  //   console.log(state);
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -19,11 +53,11 @@ const ReducerBasics = () => {
         );
       })}
 
-      {people.length < 1 ? (
+      {state.people.length < 1 ? (
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
-          onClick={() => setPeople(data)}
+          onClick={resetList}
         >
           reset
         </button>
@@ -31,7 +65,7 @@ const ReducerBasics = () => {
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
-          onClick={() => setPeople([])}
+          onClick={clearList}
         >
           clear items
         </button>
