570bf3229f7ddd60a53f4e4600d66cc2b460b7cc
veekay17 <vijay.vuex@gmail.com>
2023-07-28 18:56:12 +0530

    formatted code

--- src/redux/countSlice.js
+++ src/redux/countSlice.js
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
-    name: "counter",
-    initialState: {
-        value: 0
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state, action) => {
+      state.value += action.payload;
     },
-    reducers:{
-        increment:(state,action) => {
-            state.value += action.payload
-        },
-        decrement: (state,action) => {
-            state.value -= action.payload
-        },
-        reset: (state) => {
-            state.value = 0
-        }
-    }
-})
+    decrement: (state, action) => {
+      state.value -= action.payload;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+  },
+});
 
- const counterReducer = counterSlice.reducer
- const {increment,decrement,reset} = counterSlice.actions
- export {counterReducer,increment,decrement, reset}
+const counterReducer = counterSlice.reducer;
+const { increment, decrement, reset } = counterSlice.actions;
+export { counterReducer, increment, decrement, reset };
